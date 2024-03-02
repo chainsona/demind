316d1605a9636b0ecb9a9153cb50e4750b8bf7a3
Soona <code@chainsona.dev>
2024-03-02 20:43:40 +0100

    feat: mint cnft on onboarding

--- src/components/OnboardingAgents.tsx
+++ src/components/OnboardingAgents.tsx
@@ -2,6 +2,7 @@
 
 import { Oswald } from "next/font/google";
 import { use, useCallback, useEffect, useMemo, useState } from "react";
+import { toast } from "react-toastify";
 import { useWallet } from "@solana/wallet-adapter-react";
 
 import DialogButtons from "@/components/DialogButtons";
@@ -22,6 +23,8 @@ export default function OnboardingAgents({
   const [agents, setAgents] = useState<any[]>([]);
   const [selectedAgents, setSelectedAgents] = useState<string[]>([]);
 
+  const [loading, setLoading] = useState<boolean>(false);
+
   const userInterests = useMemo((): string[] => {
     const storageId = `demind_${wallet?.publicKey?.toBase58() || ""}`;
     const userConfig: any = JSON.parse(
@@ -33,13 +36,52 @@ export default function OnboardingAgents({
     return userConfig.interests;
   }, [wallet?.publicKey]);
 
-  function handleMint() {
+  async function handleMint() {
     let isMinted = false;
+    setLoading(true);
 
     // TODO Mint DeMinds from selected collection
-    isMinted = true;
+    if (selectedAgents.length > 0) {
+      try {
+        const res = await fetch(`/api/nfts/mint`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            projectId: 2,
+            receiverAddress: wallet?.publicKey?.toBase58(),
+          }),
+        });
+        const data = await res.json();
+
+        if (res.status === 409) {
+          toast.warn(data.error);
+          setLoading(false);
+          return;
+        }
+
+        if (res.status !== 202) {
+          toast.error(data);
+          setLoading(false);
+          return;
+        }
+        console.log("toast");
+        toast.success(`Agents minted!`);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        isMinted = true;
+      } catch (e) {
+        console.error(e);
+        setLoading(false);
+        return;
+      }
+    } else {
+      isMinted = true;
+    }
 
     if (isMinted) handleNext();
+
+    setLoading(false);
   }
 
   const fetchAgents = useCallback(async () => {
@@ -68,13 +110,13 @@ export default function OnboardingAgents({
 
   return (
     <div className="overflow-auto w-full grow flex flex-col gap-4">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
         <div
           className={`mb-2 ${oswald.className} text-4xl text-gray-200 font-bold`}
         >
           Mint your AI agents
         </div>
-        <div className={`text-sm text-gray-400 font-bold`}>
+        <div className={`text-sm text-gray-400 font-semibold`}>
           DeMinds filter the noise, bringing you actionable insights and trends
           before they hit the mainstream.
         </div>
@@ -87,12 +129,30 @@ export default function OnboardingAgents({
       />
 
       <DialogButtons
-        invalidationText="Previous"
+        invalidationText="Back"
         invalidationFn={() => handlePrevious()}
         invalidationVisible={true}
+        loading={loading}
         validationEnabled={true}
         validationFn={() => handleMint()}
-        validationText={selectedAgents.length > 0 ? "Mint" : "Skip"}
+        validationText={
+          selectedAgents.length > 0
+            ? "Mint" +
+              (selectedAgents.reduce(
+                (acc, a) =>
+                  acc + agents.find((ag) => ag.id === a)?.price.amount,
+                0
+              ) > 0
+                ? " for " +
+                  selectedAgents.reduce(
+                    (acc, a) =>
+                      acc + agents.find((ag) => ag.id === a)?.price.amount,
+                    0
+                  ) +
+                  " SOL"
+                : "")
+            : "Skip"
+        }
       />
     </div>
   );
